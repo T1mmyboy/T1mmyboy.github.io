743de33a6fb6797f793aa2fee7a36be30f77f551
t1mmyboy <112873866+t1mmyboy@users.noreply.github.com>
2023-12-18 15:47:20 +0100

    Add files via upload

--- pokedex/js.js
+++ pokedex/js.js
@@ -1,56 +1,59 @@
-const apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
-const numberOfPokemon = 649;
+document.addEventListener('DOMContentLoaded', function () {
+  const apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+  const numberOfPokemon = 649;
 
-async function fetchPokemonData() {
-  try {
-    for (let i = 1; i <= numberOfPokemon; i++) {
-      const response = await fetch(`${apiUrl}${i}`);
-      const data = await response.json();
+  async function fetchPokemonData() {
+    try {
+      for (let i = 1; i <= numberOfPokemon; i++) {
+        const response = await fetch(`${apiUrl}${i}`);
+        const data = await response.json();
 
-      // Create a card element
-      const a = document.createElement("a");
-      const card = document.createElement('div');
-      card.classList.add('pokemon-card');
+        // Create a card element
+        const a = document.createElement("a");
+        const card = document.createElement('div');
+        a.classList.add('pokemon-card');
 
-      // Display the Pokémon name
-      const nameElement = document.createElement('h2');
-      nameElement.textContent = data.name;
-      a.href = "./single.html?id="+i;
+        // Display the Pokémon name
+        const nameElement = document.createElement('h2');
+        nameElement.textContent = data.name;
+        a.href = "./single.html?id="+i;
 
-      // Display the Pokémon image
-      const imageElement = document.createElement('img');
-      imageElement.src = data.sprites.front_default;
-      imageElement.alt = data.name;
 
-      // Append elements to the card
-      card.appendChild(nameElement);
-      card.appendChild(imageElement);
+        // Display the Pokémon image
+        const imageElement = document.createElement('img');
+        imageElement.src = data.sprites.front_default;
+        imageElement.alt = data.name;
+        a.appendChild(card);
 
-      // Append the card to the pokedex-container
-      document.getElementById('pokedex-container').appendChild(a);
-      a.appendChild(card);
+        // Append elements to the card
+        card.appendChild(nameElement);
+        card.appendChild(imageElement);
+
+        // Append the card to the document body
+        document.getElementById('pokedex-container').appendChild(a);
+      }
+    } catch (error) {
+      console.error('Error fetching Pokemon data:', error);
     }
-  } catch (error) {
-    console.error('Error fetching Pokemon data:', error);
   }
-}
 
-// Function to handle search input
-function handleSearch() {
-  const searchInput = document.getElementById('searchInput');
-  const searchTerm = searchInput.value.toLowerCase();
+  // Initial fetch
+  fetchPokemonData();
 
-  const cards = document.querySelectorAll('.pokemon-card');
+  // Search functionality
+  const searchInput = document.getElementById('search');
+  searchInput.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
 
-  cards.forEach(card => {
-    const name = card.querySelector('h2').textContent.toLowerCase();
-    const isVisible = name.includes(searchTerm);
-
-    card.style.display = isVisible ? 'block' : 'none';
+    // Show/hide cards based on search term
+    const cards = document.querySelectorAll('.pokemon-card');
+    cards.forEach((a) => {
+      const name = a.querySelector('h2').textContent.toLowerCase();
+      if (name.includes(searchTerm)) {
+        a.style.display = 'block';
+      } else {
+        a.style.display = 'none';
+      }
+    });
   });
-}
-
-// Attach the handleSearch function to the input's 'input' event
-document.getElementById('searchInput').addEventListener('input', handleSearch);
-
-fetchPokemonData();
+});
